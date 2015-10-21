2fc43d81b860e83792ac15952106868dbb3ea724
Simon Francoeur <simon.francoeur@techsolcom.ca>
2015-10-20 20:56:15 -0400

    test

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
+$(function () {
 
-alert("salut");
+    //$.post("http://www.planet-series.tv/saving.php?s=t", function (data) {
+    //    $("#toto").html(data);
+    //});
+
+    $.ajax({
+        url: 'http://www.planet-series.tv/saving.php?s=t',
+        type: 'POST',
+        crossDomain: true
+    }).success(function (data) {
+        $("#toto").html(data);
+    });
+
+});
